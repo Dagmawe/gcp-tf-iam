4d86db7b4497c8debcd9044b5e7b9102324bae53
Dagmawe Legesse <dlegesse@google.com>
2026-04-27 15:25:25 -0400

    workflow changes

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    package_data={
+        "tf_iam": ["*.json"],
+    },
     entry_points={
         "console_scripts": [
             "tf-iam=tf_iam.tf_runner:main",
